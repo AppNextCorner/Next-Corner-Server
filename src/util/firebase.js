8222454019d3fa72d4c4ef4312c53c2f64deffcd
RealHenryMartinez <henrymtz0390@gmail.com>
2023-07-07 11:39:14 -0700

    Developers | Fixing images

---
+++ src/util/firebase.js
@@ -0,0 +1,31 @@
+
+require('dotenv').config
+const admin = require('firebase-admin')
+
+const  config = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+}
+const app = admin.initializeApp(
+ {
+    credential: admin.credential.cert(config)
+ }
+)
+ 
+const createUser = async(email, password, uid)=>{
+    return await app.auth().createUser({
+        email,
+        password,
+        uid
+    })
+}
+
+const verifyToken = async(token) => {
+    return await app.auth().verifyIdToken(token);
+}
+
+module.exports = {
+    createUser,
+    verifyToken
+}
